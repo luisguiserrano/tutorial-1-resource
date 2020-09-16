0dcc42e750a59ffffdabe10f959984c8b4ba9635
luisguiserrano <luisgui.serrano@gmail.com>
2020-09-16 11:52:18 -0400

    fixing setup py

--- src/python/tutorial/setup.py
+++ src/python/tutorial/setup.py
@@ -1,18 +1,9 @@
 import os
 import setuptools
 
-readme_path = os.path.join("..", "README.md")
-with open(readme_path, "r") as f:
-    long_description = f.read()
-
 setuptools.setup(
-    name                            = "tutorial-1-resource",
-    version                         = "0.1.0",
-    author                          = "Zapata Computing, Inc.",
-    author_email                    = "info@zapatacomputing.com",
+    name                            = "tutorial-resource",
     description                     = "Training models with scikit-learn in orquestra.",
-    long_description                = long_description,
-    long_description_content_type   = "text/markdown",
     url                             = "https://github.com/luisguiserrano/tutorial-1-resource",
     packages                        = setuptools.find_packages(where = "python"),
     package_dir                     = {"" : "python"},
